47a0af515cc458bd011fec738ed847dd0257c127
ashok-kuvaraja <ashok.kuvaraja@syncfusion.com>
2022-02-01 19:23:51 +0530

    Included the main file changes.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import '../view/main_page.dart';
-import 'view_model/login_view_model.dart';
+import '../src/views/main_page.dart';
+import 'src/view_models/login_view_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
